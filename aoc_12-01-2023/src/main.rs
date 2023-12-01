8ca350ea8ee5b1ac7123471885b4018ea8c46fc6
jwrunge <jwrunge@gmail.com>
2023-11-30 23:53:30 -0600

    Load from file

--- aoc_12-01-2023/src/main.rs
+++ aoc_12-01-2023/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+
+// fn first_number(input: &str) -> i32 {
+
+// }
+
+fn load_input_from_file(filename: &str) -> String {
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("File contents: {}", contents);
+    "OK".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    load_input_from_file("coords.txt");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_file() {
+        assert_eq!(load_input_from_file("coords.txt"), "OK");
+    }
 }
