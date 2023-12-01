7acd7c1f5563a1ab5b0c7d53d94e54a60696d7f8
jwrunge <jwrunge@gmail.com>
2023-12-01 00:51:38 -0600

    Word replace test works!

--- aoc_12-01-2023/src/main.rs
+++ aoc_12-01-2023/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::{fs, collections::HashMap};
 
 fn get_calibration(filename: &str) -> i32 {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
@@ -8,14 +8,14 @@ fn get_calibration(filename: &str) -> i32 {
     for line in lines {
         let first_num = first_number(line);
         let last_num = last_number(line);
-        // let combo_str = String::from(first_num + last_num);
-        // nums.push(combo_str.parse::<i32>().expect("Combo string should parse to i32"));
+        let combo_str = format!("{}{}", first_num.unwrap(), last_num.unwrap());
+        nums.push(combo_str.parse::<i32>().expect("Combo string should parse to i32"));
     }
 
     let mut sum = 0;
-    // for num in nums {
-    //     sum += num;
-    // }
+    for num in nums {
+        sum += num;
+    }
 
     sum
 }
@@ -40,8 +40,32 @@ fn last_number(input: &str) -> Option<char> {
     None
 }
 
+fn check_for_word(input: &str) -> String {
+    let words: HashMap<String, &str> = HashMap::from([
+        ("one".to_string(), "1"),
+        ("two".to_string(), "2"),
+        ("three".to_string(), "3"),
+        ("four".to_string(), "4"),
+        ("five".to_string(), "5"),
+        ("six".to_string(), "6"),
+        ("seven".to_string(), "7"),
+        ("eight".to_string(), "8"),
+        ("nine".to_string(), "9"),
+    ]);
+
+    let mut replaced = input.to_string();
+    for word in words.keys() {
+        if input.contains(word) {
+            replaced = replaced.replace(word, words.get(word).unwrap());
+        }
+    }
+
+    replaced
+}
+
 fn main() {
-    
+    let coords = get_calibration("coords.txt");
+    println!("Calibration: {}", coords);
 }
 
 #[cfg(test)]
@@ -56,5 +80,10 @@ mod tests {
     #[test]
     fn test_last_num() {
         assert_eq!(last_number(&"ab12cd34ef").unwrap(), '4');
+    }
+
+    #[test]
+    fn test_check_for_word() {
+        assert_eq!(check_for_word(&"one2ctwo34ef"), "12c234ef");
     }
 }
