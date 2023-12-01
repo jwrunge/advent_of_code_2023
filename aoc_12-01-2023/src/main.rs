ecf6ef4560bcb16639c87431b94ca96fca99acc2
jwrunge <jwrunge@gmail.com>
2023-12-01 00:29:07 -0600

    get_calibration

--- aoc_12-01-2023/src/main.rs
+++ aoc_12-01-2023/src/main.rs
@@ -1,17 +1,35 @@
-use std::fs;
+use std::{fs, str::Split};
 
-// fn first_number(input: &str) -> i32 {
-
-// }
-
-fn load_input_from_file(filename: &str) -> String {
+fn get_calibration(filename: &str) -> i32 {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("File contents: {}", contents);
-    "OK".to_string()
+    let lines = contents.split("\n");
+    
+    let mut nums: Vec<i32> = Vec::new();
+    for line in lines {
+        let first_num = first_number(line);
+        let last_num = last_number(line);
+        let combo_str = String::from(first_num + last_num);
+        nums.push(combo_str.parse::<i32>().expect("Combo string should parse to i32"));
+    }
+
+    let mut sum = 0;
+    for num in nums {
+        sum += num;
+    }
+
+    sum
+}
+
+fn first_number(input: &str) -> char {
+    
+}
+
+fn last_number(input: &str) -> char {
+    
 }
 
 fn main() {
-    load_input_from_file("coords.txt");
+    
 }
 
 #[cfg(test)]
@@ -20,6 +38,6 @@ mod tests {
 
     #[test]
     fn test_load_file() {
-        assert_eq!(load_input_from_file("coords.txt"), "OK");
+        
     }
 }
